ee898dac03b8410c91011c268c3b24f16b07e9b3
snuphy <52493191+gs18103@users.noreply.github.com>
2024-03-02 10:25:18 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
+#define fi first
+#define se second
+#define eb emplace_back
+#define em emplace
+#define all(v) v.begin(), v.end()
+#define uqv(v) v.erase(unique(all(v)), v.end())
 
 using namespace std;
+typedef long long ll;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+
+const int INF = 1e9;
+const ll LINF = 1e18;
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(0);
